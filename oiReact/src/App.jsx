d96c5ad6094a4a7edd71ca060b3a1fb5485ed430
MateusdasilvaDES <mateus_silva44@estudante.sesisenai.org.br>
2024-08-23 17:16:15 -0300

    Criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Dinossauros</h1>
+       <h2>Carnivoros</h2>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum excepturi numquam ducimus! <br/>
+        Deserunt laboriosam repudiandae maiores ad exercitationem dolorum.Ullam voluptate quo repellendus <br/>
+        laborum consequuntur quaerat nostrum omnis aliquam officiis.</p>
+    </>
+  )
+}
+
+export default App
